f0497da3d40f4c916f3f89f99279d708e116c7a2
dev-Crayon <seungheon328@gmail.com>
2024-08-08 12:35:20 +0900

    Project Initialization

---
+++ module-auth/src/main/java/com/fourcut/client/SocialType.java
@@ -0,0 +1,16 @@
+package com.fourcut.client;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SocialType {
+
+    APPLE("애플"),
+    KAKAO("카카오")
+    ;
+
+    private final String platformName;
+}
