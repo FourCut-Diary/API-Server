ec43dba968464afe5c2788ce9d9c52bb34a400b2
dev-Crayon <seungheon328@gmail.com>
2024-08-20 17:25:09 +0900

    [Feat]: 애플 ID Token 관련 에러 메세지 추가
    Related to: #3

---
+++ module-common/src/main/java/com/fourcut/diary/constant/StringConstant.java
@@ -0,0 +1,9 @@
+package com.fourcut.diary.constant;
+
+public class StringConstant {
+
+    // request validation message
+    public final static String NULL_SOCIAL_TYPE = "소셜 플랫폼 유형이 전달되지 않았습니다.";
+    public final static String INVALID_AUTHORIZATION_CODE = "처리할 수 없는 인가 코드입니다.";
+    public final static String NULL_IDENTITY_TOKEN = "애플 ID Token이 전달되지 않았습니다.";
+}
