1d327ede4a985f29d473b6a30294a90adc9f7da6
dev-Crayon <seungheon328@gmail.com>
2024-08-20 17:22:06 +0900

    [Feat]: 애플 로그인으로 인한 로그인 컨트롤러 변경
    RequestParam DTO로 관리
    login 메서드 인자에 optional로 identityToken 추가
    
    Related to: #3

--- module-api/src/main/java/com/fourcut/diary/user/UserController.java
+++ module-api/src/main/java/com/fourcut/diary/user/UserController.java
@@ -1,15 +1,14 @@
 package com.fourcut.diary.user;
 
-import com.fourcut.diary.client.SocialType;
 import com.fourcut.diary.strategy.SocialStrategy;
 import com.fourcut.diary.strategy.SocialStrategyProvider;
 import com.fourcut.diary.strategy.dto.SocialLoginResponse;
+import com.fourcut.diary.strategy.dto.SocialLoginRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +19,9 @@ public class UserController {
     private final SocialStrategyProvider socialStrategyProvider;
 
     @PostMapping("/social-login")
-    public ResponseEntity<SocialLoginResponse> socialLogin(@RequestParam final SocialType socialType, @RequestParam final String authorizationCode) {
-        SocialStrategy socialStrategy = socialStrategyProvider.getSocialService(socialType);
+    public ResponseEntity<SocialLoginResponse> socialLogin(final SocialLoginRequest request) {
+        SocialStrategy socialStrategy = socialStrategyProvider.getSocialService(request.socialType());
 
-        return ResponseEntity.status(HttpStatus.OK).body(socialStrategy.login(authorizationCode));
+        return ResponseEntity.status(HttpStatus.OK).body(socialStrategy.login(request));
     }
 }
