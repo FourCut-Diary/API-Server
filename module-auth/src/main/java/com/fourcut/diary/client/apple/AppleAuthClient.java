3342cb2d18295367e732921b53ccf2be8633cb07
dev-Crayon <seungheon328@gmail.com>
2024-08-27 17:31:52 +0900

    [Refactor]: 사용하지 않는 코드 및 파일 삭제
    Related to: #3

--- module-auth/src/main/java/com/fourcut/diary/client/apple/AppleAuthClient.java
+++ module-auth/src/main/java/com/fourcut/diary/client/apple/AppleAuthClient.java
@@ -1,17 +1,12 @@
 package com.fourcut.diary.client.apple;
 
 import com.fourcut.diary.client.apple.dto.AppleOAuth2TokenResponse;
-import com.fourcut.diary.client.apple.dto.ApplePublicKeyResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "appleAuthClient", url = "https://appleid.apple.com")
 public interface AppleAuthClient {
-
-    @GetMapping("/auth/keys")
-    ApplePublicKeyResponse getApplePublicKey();
 
     @PostMapping("/auth/token")
     AppleOAuth2TokenResponse getOAuth2Token(
--- module-auth/src/main/java/com/fourcut/diary/strategy/apple/AppleTokenUtil.java
+++ module-auth/src/main/java/com/fourcut/diary/strategy/apple/AppleTokenUtil.java
@@ -1,59 +1,15 @@
 package com.fourcut.diary.strategy.apple;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fourcut.diary.client.apple.AppleAuthClient;
-import com.fourcut.diary.client.apple.dto.ApplePublicKey;
-import com.fourcut.diary.client.apple.dto.ApplePublicKeyResponse;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.naming.AuthenticationException;
-import java.math.BigInteger;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
 import java.util.Base64;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class AppleTokenUtil {
-
-    private final AppleAuthClient appleAuthClient;
-
-    public Claims getClaimsByIdentityToken(String identityToken) {
-        try {
-            ApplePublicKeyResponse response = appleAuthClient.getApplePublicKey();
-            String headerOfIdentityToken = identityToken.substring(0, identityToken.indexOf("."));
-            Map<String, String> header = stringHeaderToMap(headerOfIdentityToken);
-            ApplePublicKey applePublicKey = response.getMatchedKey(header.get("kid"), header.get("alg"));
-
-            byte[] nBytes = Base64.getUrlDecoder().decode(applePublicKey.n());
-            byte[] eBytes = Base64.getUrlDecoder().decode(applePublicKey.e());
-
-            BigInteger n = new BigInteger(1, nBytes);
-            BigInteger e = new BigInteger(1, eBytes);
-
-            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(n, e);
-            KeyFactory keyFactory = KeyFactory.getInstance(applePublicKey.kty());
-            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
-
-            return Jwts.parserBuilder()
-                    .setSigningKey(publicKey)
-                    .build()
-                    .parseClaimsJws(identityToken)
-                    .getBody();
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException | AuthenticationException exception) {
-            System.out.println(exception.getMessage());
-            throw new RuntimeException(exception);
-        }
-    }
 
     public <T> T decodePayload(String token, Class<T> targetClass) {
 
@@ -68,18 +24,6 @@ public class AppleTokenUtil {
             return objectMapper.readValue(payload, targetClass);
         } catch (Exception e) {
             throw new RuntimeException("Error decoding token payload", e);
-        }
-    }
-
-    private Map<String, String> stringHeaderToMap(String headerOfIdentityToken) {
-        try {
-            byte[] decodedBytes = Base64.getUrlDecoder().decode(headerOfIdentityToken);
-            String decodedString = new String(decodedBytes);
-
-            return new ObjectMapper().readValue(decodedString, new TypeReference<>() {
-            });
-        } catch (Exception exception) {
-            throw new RuntimeException("Error decoding token header", exception);
         }
     }
 }
