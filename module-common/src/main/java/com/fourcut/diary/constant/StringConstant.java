f48838326b80dc52559ad27189d38ea41d5bbb1e
dev-Crayon <seungheon328@gmail.com>
2024-09-08 16:51:55 +0900

    [Feat]: 유저 로그인 및 회원가입 Validation String 정의
    request body에 넘어오는 데이터 검증 실패 문자열 추가
    
    Related to: #4

--- module-common/src/main/java/com/fourcut/diary/constant/StringConstant.java
+++ module-common/src/main/java/com/fourcut/diary/constant/StringConstant.java
@@ -2,8 +2,15 @@ package com.fourcut.diary.constant;
 
 public class StringConstant {
 
-    // request validation message
+    // auth
     public final static String NULL_SOCIAL_TYPE = "소셜 플랫폼 유형이 전달되지 않았습니다.";
     public final static String INVALID_AUTHORIZATION_CODE = "처리할 수 없는 인가 코드입니다.";
     public final static String INVALID_FCM_TOKEN = "처리할 수 없는 디바이스 토큰입니다.";
+
+    // user
+    public final static String INVALID_USER_NAME = "허용되지 않은 형식의 이름입니다.";
+    public final static String INVALID_NICKNAME = "허용되지 않은 형식의 닉네임입니다.";
+    public final static String NULL_BIRTHDAY = "생일이 입력되지 않았습니다.";
+    public final static String NULL_GENDER = "성별이 입력되지 않았습니다.";
+
 }
