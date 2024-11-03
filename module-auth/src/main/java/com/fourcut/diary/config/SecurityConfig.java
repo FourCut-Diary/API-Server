808ffc85baac337fc1933a09cd5686814fb56e41
dev-Crayon <seungheon328@gmail.com>
2024-11-03 19:50:30 +0900

    [Feat]: security 설정 및 유저 인증 어노테이션 구현
    UserAuthenticationPrincipleResolver 구현
    security filter 추가
    
    Related to: #17

--- module-auth/src/main/java/com/fourcut/diary/config/SecurityConfig.java
+++ module-auth/src/main/java/com/fourcut/diary/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.fourcut.diary.config;
 
+import com.fourcut.diary.filter.CustomJwtAuthenticationFilter;
+import com.fourcut.diary.filter.JwtExceptionHandlerFilter;
+import com.fourcut.diary.jwt.JwtTokenManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,11 +11,16 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenManager jwtTokenManager;
+
     private static final String[] AUTH_WHITELIST = {
+            "/error",
             "/user/social-signup",
             "/user/social-login"
     };
@@ -34,6 +43,10 @@ public class SecurityConfig {
                     header
                             .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin);
                 });
+
+        http
+                .addFilterBefore(new CustomJwtAuthenticationFilter(jwtTokenManager), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionHandlerFilter(), CustomJwtAuthenticationFilter.class);
 
         return http.build();
     }
