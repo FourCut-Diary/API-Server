67378b13169478736422a2fa4815815747eb9145
dev-Crayon <seungheon328@gmail.com>
2024-11-03 19:49:19 +0900

    [Feat]: 토큰 검증을 위한 security filter 구현
    CustomJwtAuthenticationFilter
    CustomUserDetails
    CustomUserDetailsService
    JwtExceptionHandlerFilter
    
    Related to: #17

---
+++ module-auth/src/main/java/com/fourcut/diary/filter/CustomJwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.fourcut.diary.filter;
+
+import com.fourcut.diary.jwt.JwtTokenManager;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class CustomJwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenManager jwtTokenManager;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String accessToken = resolveAccessToken(request);
+
+        if (accessToken != null && jwtTokenManager.validateToken(accessToken)) {
+            Authentication authentication = jwtTokenManager.getAuthentication(accessToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveAccessToken(HttpServletRequest request) {
+
+        return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .filter(token -> token.substring(0, 7).equalsIgnoreCase("Bearer "))
+                .map(token -> token.substring(7))
+                .orElse(null);
+    }
+}
---
+++ module-auth/src/main/java/com/fourcut/diary/filter/CustomUserDetails.java
@@ -0,0 +1,57 @@
+package com.fourcut.diary.filter;
+
+import com.fourcut.diary.user.domain.RoleType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private String socialId;
+    private RoleType role;
+
+    public CustomUserDetails(String socialId, RoleType role) {
+        this.socialId = socialId;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.socialId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
