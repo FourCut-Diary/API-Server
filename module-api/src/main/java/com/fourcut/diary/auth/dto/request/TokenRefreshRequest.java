882438d00958167a027997ad4d6bcaaedf84392a
dev-Crayon <seungheon328@gmail.com>
2024-11-08 15:15:51 +0900

    [Feat]: 토큰 재발급 request validation 추가
    Related to: #20

--- module-api/src/main/java/com/fourcut/diary/auth/dto/request/TokenRefreshRequest.java
+++ module-api/src/main/java/com/fourcut/diary/auth/dto/request/TokenRefreshRequest.java
@@ -1,7 +1,11 @@
 package com.fourcut.diary.auth.dto.request;
 
+import com.fourcut.diary.constant.ValidationMessage;
+import jakarta.validation.constraints.NotBlank;
+
 public record TokenRefreshRequest(
 
+        @NotBlank(message = ValidationMessage.INVALID_REFRESH_TOKEN)
         String refreshToken
 ) {
 }
