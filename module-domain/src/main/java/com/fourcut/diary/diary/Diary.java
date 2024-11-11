807a587e056510db458f4ad627f4cca3a64a9924
dev-Crayon <seungheon328@gmail.com>
2024-11-11 17:36:31 +0900

    [Feat]: 일기 Entity class 정의
    Related to: #23

---
+++ module-domain/src/main/java/com/fourcut/diary/diary/Diary.java
@@ -0,0 +1,50 @@
+package com.fourcut.diary.diary;
+
+import com.fourcut.diary.common.AuditingTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Diary extends AuditingTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column
+    private String firstPicture;
+
+    @Column
+    private String secondPicture;
+
+    @Column
+    private String thirdPicture;
+
+    @Column
+    private String fourthPicture;
+
+    @Column
+    private String firstComment;
+
+    @Column
+    private String secondComment;
+
+    @Column
+    private String thirdComment;
+
+    @Column
+    private String fourthComment;
+
+}
