763173328253f504b238f74866edd95a9c236d2e
dev-Crayon <seungheon328@gmail.com>
2024-12-16 18:52:14 +0900

    [Feat]: 사진찍기 정보 로직 util함수로 분리
    Related to: #33

--- module-domain/src/main/java/com/fourcut/diary/user/service/notification/NotificationTimeService.java
+++ module-domain/src/main/java/com/fourcut/diary/user/service/notification/NotificationTimeService.java
@@ -3,18 +3,16 @@ package com.fourcut.diary.user.service.notification;
 import com.fourcut.diary.user.domain.User;
 import com.fourcut.diary.user.domain.notification.NotificationTime;
 import com.fourcut.diary.user.service.dto.TakePhotoInfoDto;
+import com.fourcut.diary.user.util.NotificationTimeUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationTimeService {
-
-    private static final int EXPIRATION_MINUTES = 20;
 
     private final NotificationTimeRetriever notificationTimeRetriever;
 
@@ -22,28 +20,6 @@ public class NotificationTimeService {
     public TakePhotoInfoDto getTakePhotoInfoByUser(User user) {
         NotificationTime notificationTime = notificationTimeRetriever.findNotificationTimeByUser(user);
         LocalDateTime now = LocalDateTime.now();
-        return calculatePhotoInfo(notificationTime, now);
-    }
-
-    private TakePhotoInfoDto calculatePhotoInfo(NotificationTime notificationTime, LocalDateTime now) {
-        List<LocalDateTime> timeSlots = List.of(
-                notificationTime.getFirstTimeSlot(),
-                notificationTime.getSecondTimeSlot(),
-                notificationTime.getThirdTimeSlot(),
-                notificationTime.getFourthTimeSlot()
-        );
-
-        for (int i = 0; i < timeSlots.size(); i++) {
-            LocalDateTime slot = timeSlots.get(i);
-            if (getIsPossiblePhotoCapture(slot, now)) {
-                return new TakePhotoInfoDto(i + 1, slot.plusMinutes(EXPIRATION_MINUTES).toLocalTime());
-            }
-        }
-        return new TakePhotoInfoDto(-1, null);
-    }
-
-    private Boolean getIsPossiblePhotoCapture(LocalDateTime beginningTime, LocalDateTime now) {
-        LocalDateTime expirationTime = beginningTime.plusMinutes(20);
-        return !now.isBefore(beginningTime) && !now.isAfter(expirationTime);
+        return NotificationTimeUtil.getTakePhotoInfo(notificationTime, now);
     }
 }
