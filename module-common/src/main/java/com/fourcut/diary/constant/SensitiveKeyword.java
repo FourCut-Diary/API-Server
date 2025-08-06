564deb76a2048f9aaa0503bd0cc9e70141119d8f
Lee SeungHeon <51286325+dev-Crayon@users.noreply.github.com>
2025-08-06 18:14:35 +0900

    Spring AOP를 활용하여 request & response 로깅 (#68)
    * [Feat]: Spring AOP 의존성 추가 및 LoggingConfig 설정
    
    Spring AOP 의존성 추가
    LoggingConfig 설정
    
    Related to: #67
    
    * [Feat]: request & response 로깅 적용
    
    redis repository 범위 지정
    request & response 로깅
    
    Related to: #67

---
+++ module-common/src/main/java/com/fourcut/diary/constant/SensitiveKeyword.java
@@ -0,0 +1,6 @@
+package com.fourcut.diary.constant;
+
+public class SensitiveKeyword {
+
+    public static final String[] SENSITIVE_KEYWORDS = {"authorizationCode", "fcmToken", "accessToken", "refreshToken", "presignedUrl"};
+}
---
+++ module-common/src/main/java/com/fourcut/diary/logging/LoggingConfig.java
@@ -0,0 +1,9 @@
+package com.fourcut.diary.logging;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingConfig {
+}
